0482c279a1eed0cca77ed53820254f8a9539aa59
yigitcanolmez <yigitcanolmez@outlook.com>
2023-10-13 22:15:51 +0300

    extension Middleware

--- MiddlewareSection/ExampleMiddleware/exampleOfMiddleware/Program.cs
+++ MiddlewareSection/ExampleMiddleware/exampleOfMiddleware/Program.cs
@@ -6,18 +6,6 @@ builder.Services.AddTransient<MyCustomMiddleware>();
 
 var app = builder.Build();
 
-app.Use(async (HttpContext context, RequestDelegate next) =>
-{
-    await context.Response.WriteAsync("First Middleware!");
-
-    await next(context);
-});
-
-app.UseMiddleware<MyCustomMiddleware>();
-
-app.Run(async (HttpContext context) => {
-
-    await context.Response.WriteAsync("Merhaba!"); //app.run ile middleware yazılırsa, tek bir middleware olur doğal olarak altına başka bir app run ile middleware eklenirse çalışmaz
-});
+app.UseExampleOfMiddleware();
 
 app.Run();
---
+++ MiddlewareSection/ExampleMiddleware/exampleOfMiddleware/UseExampleMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace exampleOfMiddleware
+{
+	public static class UseExampleMiddleware 
+	{
+	
+        public static IApplicationBuilder UseExampleOfMiddleware(this IApplicationBuilder app)
+		{
+            app.Use(async (HttpContext context, RequestDelegate next) =>
+            {
+                await context.Response.WriteAsync("First Middleware!");
+
+                await next(context);
+            });
+
+            app.UseMiddleware<MyCustomMiddleware>();
+
+            app.Run(async (HttpContext context) => {
+
+                await context.Response.WriteAsync("Merhaba!"); //app.run ile middleware yazılırsa, tek bir middleware olur doğal olarak altına başka bir app run ile middleware eklenirse çalışmaz
+            });
+            return app;
+
+		}
+
+	}
+}
+
