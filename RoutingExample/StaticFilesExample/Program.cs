d44211961e747cb7c3ce9fbd84e72e173525bf88
yigitcanolmez <yigitcanolmez@outlook.com>
2023-10-15 13:54:25 +0300

    learnt Static Files

---
+++ RoutingExample/StaticFilesExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
+{
+    WebRootPath = "myRoot"
+});
+var app = builder.Build();
+
+app.UseStaticFiles();//myRoot
+
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "myWebRoot"))
+});
+
+//http://localhost:5175/ocelot.png şeklinde yazarsan static file açılır:)))))))
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.Map("/", async (context) =>
+    {
+        context.Response.WriteAsync("Hello");
+    });
+});
+app.Run();
+
