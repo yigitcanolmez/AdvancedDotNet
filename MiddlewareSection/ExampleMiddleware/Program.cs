091196316a6cecdec6e95a0e87cb41c24912f252
yigitcanolmez <yigitcanolmez@outlook.com>
2023-10-15 11:22:07 +0300

    fix

--- MiddlewareSection/ExampleMiddleware/Program.cs
+++ MiddlewareSection/ExampleMiddleware/Program.cs
@@ -1,15 +1,15 @@
-﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+﻿//var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+//var app = builder.Build();
 
-app.Use(async (HttpContext context, RequestDelegate next) =>
-{
-    await context.Response.WriteAsync("First Middleware!");
+//app.Use(async (HttpContext context, RequestDelegate next) =>
+//{
+//    await context.Response.WriteAsync("First Middleware!");
 
-    await next(context);
-});
+//    await next(context);
+//});
 
-app.Run(async (HttpContext context) => {
+//app.Run(async (HttpContext context) => {
 
-    await context.Response.WriteAsync("Merhaba!"); //app.run ile middleware yazılırsa, tek bir middleware olur doğal olarak altına başka bir app run ile middleware eklenirse çalışmaz
-});
+//    await context.Response.WriteAsync("Merhaba!"); //app.run ile middleware yazılırsa, tek bir middleware olur doğal olarak altına başka bir app run ile middleware eklenirse çalışmaz
+//});
