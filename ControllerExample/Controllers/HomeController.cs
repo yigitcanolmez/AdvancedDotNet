a92122bb1374466a1021a41e9dfeada031982935
yigitcanolmez <yigitcanolmez@outlook.com>
2023-10-16 22:18:03 +0300

    json Result

---
+++ ControllerExample/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerExample.Controllers
+{
+    [Controller]
+    class HomeController :Controller
+    {
+        public HomeController()
+        {
+
+        }
+        [Route("/")]
+        public ContentResult Index()
+        {
+            return new ContentResult
+            {
+                Content ="Hello!",
+                ContentType = "text/plain",
+                StatusCode = StatusCodes.Status200OK
+            };
+
+        }
+
+    }
+
+}
+
--- ControllerExample/Program.cs
+++ ControllerExample/Program.cs
@@ -1,7 +1,10 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
+
 var app = builder.Build();
 
 app.MapControllers();
+
 app.Run();
 
